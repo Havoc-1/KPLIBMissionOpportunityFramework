962e860c17d353066f81ddece0f13d78e36fb804
havoc-1 <syguico@gmail.com>
2023-10-24 10:51:31 +0800

    Notification System Inclusion
    - Included LMO Notifications into liberations CfgNotifications hpp file

---
+++ liberation_notifications.hpp
@@ -0,0 +1,15 @@
+class CfgNotifications {
+    class LMOTask {
+		title = "Mission of Opportunity";
+		iconPicture = "%2";
+		description = "%1";
+		priority = "7";
+	};
+
+    class LMOTaskOutcome {
+        title = "Mission of Opportunity";
+		iconPicture = "%2";
+		description = "%1";
+		priority = "7";
+    };
+};
